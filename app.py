e2e509394129d56f3c873c0c42e0112dcf991892
GROH018 <stefan.grohnert@bertelsmann.de>
2023-11-29 09:43:40 +0100

    Added forecast endpoint with template for TimeGPT support

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
 
+import pandas as pd
+
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
+from nixtlats import TimeGPT
 
 app = Flask(__name__)
 
@@ -34,6 +37,39 @@ def calc():
             data = request_json["data"]
             print('Request for calc page received with data=%s' % data)
             response_json = {"data":str(data)+" modified"}
+            return response_json
+        else:
+            print('Request for calc page received with no name or blank name -- redirecting')
+            response_json = {"data":"data was empty"}
+            return response_json
+    else:
+        response_json = {"data":"request again without data"}
+        return response_json
+
+
+@app.route('/forecast', methods=['POST'])
+def forecast():
+    if request.data.decode():
+        request_json = request.get_json()
+        if "data" in request_json:
+            # Get data and transform to dataframe
+            df = pd.DataFrame(request_json.data)
+
+            # Init TimeGPT API access
+            timegpt = TimeGPT(
+                # defaults to os.environ.get("TIMEGPT_TOKEN")
+                token='my_token_provided_by_nixtla'
+            )
+
+            # Forecast with TimeGPT
+            fcst_df = timegpt.forecast(df, h=24, level=[80, 90])
+
+            # Retrieve data from response
+            data = request_json["data"]
+            print('Request for calc page received with data=%s' % data)
+
+            # Prepare data for response
+            response_json = {"forecast":str(data)+" forecast"}
             return response_json
         else:
             print('Request for calc page received with no name or blank name -- redirecting')
